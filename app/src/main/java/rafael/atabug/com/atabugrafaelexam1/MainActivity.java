3be90ebeab9ad6bce5a0bb0ae962d1f47eed8c2d
rafaelatabug123 <2015081032@ust-ics.mygbiz.com>
2018-10-06 15:53:30 +0800

    Initial commit

---
+++ app/src/main/java/rafael/atabug/com/atabugrafaelexam1/MainActivity.java
@@ -0,0 +1,19 @@
+package rafael.atabug.com.atabugrafaelexam1;
+
+import android.app.SearchManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
